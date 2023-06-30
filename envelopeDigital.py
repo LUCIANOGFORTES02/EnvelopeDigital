71f439ef0bc576d97c5b8195884a2da31b0aac68
LUCIANOGFORTES02 <lucianogfortes@gmail.com>
2023-06-30 00:15:11 -0300

    rsa

--- envelopeDigital.py
+++ envelopeDigital.py
@@ -16,25 +16,33 @@ Saída
 Dois arquivos um com a chave assinada e outro do arquivo criptografado
 """
 from cryptography.fernet import Fernet
+import rsa
 
 
-def envelopeDigital ():
+def envelopeDigital (arquivoEmClaro,arquivoChavePublica,algoritmoSimetrico):
+
+#Gerar chave simetrica com o algoritmo passado 
+
+#Cifrar o arquivo em claro com a chave simetrica
     
+#Cifrar a chave simetrica com a chave publica do destinatário
     
     return 0
 
 
 
 
+#Gerar uma chave rsa
+chavePrivada,chavePublica=rsa.newkeys(2048)#Chaves do destinatario
 
-#Gerando chave aleatória
-key =Fernet.generate_key()
-f = Fernet(key)
+
+#Criar aqruivo com a chave pública do destinatario
+with open('chavePublicaDest.txt', 'wb') as chave_assinada_arquivo:
+        chave_assinada_arquivo.write(chavePrivada.save_pkcs1())
+
+
 
 #Lendo o arquivo em claro
-file = open("textoClaro.txt","rb")
-data = file.read()
-
-#Criptografando o arquivo
-encrypted = f.encrypt(data)
-print (encrypted)
+with open ("textoClaro.txt","r") as file:
+     data= file.read()
+print (data)
